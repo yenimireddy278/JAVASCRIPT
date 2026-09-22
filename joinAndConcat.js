a7b69d90c2e85fc7a66dbe06c53a3e4e6a4dfe06
Yenimireddy Vignesh <vignesh22032006@gmail.com>
2026-09-22 14:04:26 +0530

    update

---
+++ joinAndConcat.js
@@ -0,0 +1,8 @@
+let fruits=["Apple","Banana","Mango"];
+console.log(fruits);
+let result=fruits.join(" ");
+console.log(result);
+let numbers1=[10,20];
+let numbers2=[30,40];
+let result2=numbers1.concat(numbers2);
+console.log(result2);
